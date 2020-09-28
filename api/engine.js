601ceb40a6e39b9e50fb5fa5e202cba5f827af33
Damilola <38119651+damilola5468@users.noreply.github.com>
2020-09-28 06:18:45 -0700

    Update engine.js

--- api/engine.js
+++ api/engine.js
@@ -44,7 +44,7 @@ app.get("*", function(req, res) {
 // });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello World");
+  res.json("Hello World");
 });
 
 app.post("/c/products", (req, res) => {
